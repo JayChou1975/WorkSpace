502da4c5bc0e16a69cf8500612c4fad64f426ebc
JayChou1975 <jaychou1975@outlook.com>
2022-10-08 09:56:42 +0800

    [1025] 除数博弈

---
+++ CWorkSpace/1025.除数博弈.c
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=c
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+
+
+bool divisorGame(int n){
+    if(n%2==0) return 1;
+    return 0;
+
+    // n=3 false
+    // n==4 true
+    // =5
+
+    // 不断用 n-x 替代 n ，当替代次数为奇数次时，返回1
+
+
+}
+// @lc code=end
+
